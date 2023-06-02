bcd2fc742d24bd983b6a5a909c9f640b3a8c6860
hypo368 <114984095+hypo368@users.noreply.github.com>
2023-06-02 13:03:41 +0200

    reformate les dossier

--- projet/cli/our_dir.asm
+++ projet/cli/our_dir.asm
@@ -18,14 +18,13 @@ includelib c:\masm32\lib\msvcrt.lib
 ; variables initialisees
     searchHandle dd 0
     findData WIN32_FIND_DATA <>
-    szPath     db    'F:\Images\*',0
+    szPath     db    "F:\Images\*",0
     phTest  db    "AAAAA",10,0
     formatString db "%s",10,0
-    fullPath db 1024 dup(0)
+    fullPath db "F:\Images\*",0
     dot db ".", 0
     dotdot db "..", 0
-    tes db "here", 0
-
+	strCommand db "Pause",13,10,0
 
 
 .DATA?
@@ -43,8 +42,12 @@ our_dir:
     invoke FindFirstFile, ecx, addr findData
     mov searchHandle, eax
     ; Print file name
-    
-
+    invoke FindNextFile, searchHandle, addr findData
+    invoke FindNextFile, searchHandle, addr findData
+	cmp eax, 0
+	je fin
+	
+	
     test findData.dwFileAttributes, FILE_ATTRIBUTE_DIRECTORY
     jnz ItsADirectory ; si c'est un dossier on appelle la fonction d
     ;si le dossier n'est pas . ou .. on affiche le nom du fichier
@@ -55,9 +58,15 @@ our_dir:
     our_next_file:
         invoke FindNextFile, searchHandle, addr findData
         cmp eax, 0
-        jne our_dir ; appel récursif
+		jne testDirectory
         ; sinon c'est ciao    
         jmp fin
+		
+	testDirectory:
+		test findData.dwFileAttributes, FILE_ATTRIBUTE_DIRECTORY
+		jnz ItsADirectory ; si c'est un dossier on appelle la fonction d
+		jmp our_next_file
+
 
     
     ItsADirectory:
@@ -65,27 +74,23 @@ our_dir:
         
         ;jmp our_dir currPath ;ou autre avec en input le path du nouveau dossier 
         ;currPath : le chemin que l'on veut explorer
-
+        mov byte ptr [fullPath + 1023], 0 ; on met le dernier byte a 0
         lea eax, fullPath ; on recup le path
         push eax ; YEET sur la pile
-        push offset findData.cFileName; YEET nom du dossier courant
+        push offset findData.cFileName ; on met le nom du dossier sur la pile
         call our_strcat ; fuuuuUUUUUUSIUOONNNNN (oe la ref DBZ tavu)
-        add esp, 8 ; nettoyage de la pile car on est les boniches de l'assembleur
+        
+
         ; et on recommence avec le \*
-
-        push eax
-        push "\*"
+        
+        push "\\*"
         call our_strcat
-        add esp, 8
-
-        push eax
-        push 0h
-        call our_strcat
-        add esp, 8
+        mov eax, esp ; on recup le path
 
         push eax ; on met le path sur la pile
-        call r_dir ; et c'est reparti pour un tour
+        call our_dir ; et c'est reparti pour un tour
             
+
     fin:
         ; Close the search handle
         invoke FindClose, searchHandle
@@ -97,68 +102,51 @@ our_dir:
         invoke ExitProcess, 0
 
 ; Routine strcat en assembly
-
-r_dir:
-    ; Find the first file
-    invoke FindFirstFile, addr szPath, addr findData
-    invoke FindNextFile, searchHandle, addr findData ; on passe . et ..
-    mov searchHandle, eax
-    
-    ; Loop through all files
-    boucle:
-        ; Print file name
-        invoke crt_printf, addr formatString, addr findData.cFileName
-        ; Find the next file
-        invoke FindNextFile, searchHandle, addr findData
-        ;si on trouve un dossier on appelle la fonction
-        test findData.dwFileAttributes, FILE_ATTRIBUTE_DIRECTORY
-        jnz ItsADirectory ; la fin call our_dir
-
-
-        cmp eax, 0;si ya plus de fichier
-        jne boucle
-
-    ; Close the search handle
-    invoke FindClose, searchHandle
-
-
-
-
 our_strcat:
-    ; Sauvegarder les registres que nous allons utiliser
-    push esi
-    push edi
+    push ebp              ; Save the base pointer
+    mov ebp, esp          ; Set up a new stack frame
 
-    ; Prendre les deux chaînes à partir de la pile
-    mov esi, [esp + 12]  ; source (chaîne à ajouter)
-    mov edi, [esp + 16]  ; destination (chaîne à modifier)
+    ; Retrieve the source and destination strings from the stack
+    mov esi, [ebp+8]      ; source (string to be added)
+    mov edi, [ebp+12]     ; destination (string to be modified)
 
-    ; Trouver le caractère de fin (0x00) dans la destination
-    xor eax, eax
-    not eax
-    cld
-    repne scasb
-    dec edi
+    ; Find the end of the destination string
+    mov ecx, edi
+    xor al, al            ; Set AL to null byte (0x00)
+    repne scasb           ; Search for the null byte in the destination string
+    dec edi               ; Decrement edi to point to the null byte
 
-    ; Copier la source à la fin de la destination
-    xor ecx, ecx
-    not ecx
-    repne scasb
-    not ecx
-    dec ecx
-    mov esi, [esp + 12]
-    rep movsb
+    ; Copy the source string to the end of the destination string
+    call count            ; load the lenght of esi into ecx
+    mov esi, [ebp+8]      ; Reset esi to point to the source string
+    xor al, al            ; Set AL to null byte (0x00)
+    rep movsb             ; Copy the source string to the destination string // movsb boucle un nombre de fois égal a la valeur de ecx !
 
-    ; Restaurer les registres et quitter
-    pop edi
-    pop esi
-    ret 8
+    pop ebp               ; Restore the base pointer
+    ret                   ; Return from the function
 
+
+count: 
+    mov ecx,0 ; initialisation du compteur
+
+    loop_start: 
+        mov al, [esi] ; charge la valeur de esi dans al
+        cmp al, 0
+        je loop_end
+
+        inc ecx
+        inc esi
+        jmp loop_start
+
+    loop_end: 
+        ; on ne fait rien puisque la valeur du compteur est dans le regisre ecx
+        ret
 start:
-    push offset szPath
-    call r_dir
-
     
+    push offset szPath
+    call our_dir
+	invoke crt_system, offset strCommand
+
     invoke ExitProcess, 0
 
 
--- projet/make.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-c:\masm32\bin\ml /c /Zd /coff our_dir.asm
-c:\\masm32\bin\Link /SUBSYSTEM:CONSOLE our_dir.obj
-our_dir.exe
-pause
